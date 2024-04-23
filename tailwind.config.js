21b954c8f7c9065e45b987a50165144904098abb
Jared Holzman <jared@holzman.io>
2024-04-23 15:43:42 -0700

    Started playing around with styles.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,22 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#0098ff",
+          "secondary": "#505000",
+          "accent": "#007300",
+          "neutral": "#1d170e",
+          "base-100": "#fdfff9",
+          "info": "#00d4ec",
+          "success": "#00ad85",
+          "warning": "#cb0000",
+          "error": "#ca003e",
+        },
+      }
+    ],
+  },
 }
 
