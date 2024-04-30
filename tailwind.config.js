afd37d49ed0ab74d872b7e0d85b1d26ab24c2bbd
Jared Holzman <jared@holzman.io>
2024-04-29 22:45:58 -0700

    Standardize nav and footer.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,7 @@ module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
     extend: {
-      gridTemplateRows: {
-
-      }
+      gridTemplateRows: {},
     },
   },
   plugins: [require("daisyui")],
@@ -13,21 +11,22 @@ module.exports = {
     themes: [
       {
         mytheme: {
-          "primary": "#B8CEFD",
-          "secondary": "#2E4892",
-          "accent": "#265EFF",
-          "neutral": "#ffffff",
-          "base-100": "#f3f4f6",
-          "info": "#19bcff",
-          "success": "#00ca00",
-          "warning": "#ff9e00",
-          "error": "#ff2b77",
+          primary: "#B8CEFD",
+          secondary: "#2E4892",
+          accent: "#265EFF",
+          neutral: "#ffffff",
+          "base-100": "#070D1D",
+          info: "#19bcff",
+          success: "#00ca00",
+          warning: "#ff9e00",
+          error: "#ff2b77",
         },
-      }
+      },
     ],
   },
-}
+};
 
 // Blueish-white text color: #dee1f9
 // Darker blue bg: #2E4892
 // Lighter blue bg: #d1e2ff
+// Old base-100: #f3f4f6
