19f750217f483bd52d7e50cb7ea69700692d329a
Jared Holzman <jared@holzman.io>
2024-06-04 09:32:23 -0700

    Squashed commit containing updated landing and additional animations.
    commit 634ab8989b18f33dccf4dcd95ae791dff3e6b234
    Author: Jared Holzman <jared@holzman.io>
    Date:   Tue Jun 4 06:08:08 2024 -0700
    
        Animate features section.
    
    commit 5ea91a89284852437c8ee99cf48fd910491ccaa0
    Author: Jared Holzman <jared@holzman.io>
    Date:   Tue Jun 4 05:40:57 2024 -0700
    
        Navbar tweaks.
    
    commit 09f33dac6d8b4d3cc1bad4b10ecc440c952185f4
    Author: Jared Holzman <jared@holzman.io>
    Date:   Tue Jun 4 04:00:04 2024 -0700
    
        Seperate front layer.
    
    commit f7a457f3e6c588d2a73ba98e9b241ed429704836
    Author: Jared Holzman <jared@holzman.io>
    Date:   Sun Jun 2 15:16:30 2024 -0700
    
        Add forest svg landing.
    
    commit eaa8883227af00d72fe603a2d1b8fcf13522c214
    Author: Jared Holzman <jared@holzman.io>
    Date:   Fri May 31 14:26:48 2024 -0700
    
        Add photo and font.
    
    commit e08a1c910c8e1274019f3788a9cfc934010ad8fe
    Author: Jared Holzman <jared@holzman.io>
    Date:   Wed May 29 22:50:09 2024 -0700
    
        Delete unused styles.
    
    commit 68de4db988f7e2582e31cdd86b28abb9aa2202e5
    Author: Jared Holzman <jared@holzman.io>
    Date:   Wed May 29 22:48:35 2024 -0700
    
        Initial curve design.

--- src/js/scroll-observer.js
+++ src/js/scroll-observer.js
@@ -2,17 +2,17 @@ const handleScroll = (entries) => {
   entries.forEach((entry) => {
     console.log(entry);
     if (entry.isIntersecting) {
-      entry.target.classList.add("timeline-animate");
-      entry.target.classList.remove("timeline-pre-animate");
+      entry.target.classList.add("scroll-animate");
+      entry.target.classList.remove("scroll-pre-animate");
     } else {
-      entry.target.classList.remove("timeline-animate");
-      entry.target.classList.add("timeline-pre-animate");
+      entry.target.classList.remove("scroll-animate");
+      entry.target.classList.add("scroll-pre-animate");
     }
   });
 };
 
 const observer = new IntersectionObserver(handleScroll);
-const timelineAnimate = document.querySelectorAll(".timeline-pre-animate");
+const timelineAnimate = document.querySelectorAll(".scroll-pre-animate");
 // const timelineIcons = document.querySelectorAll(".timeline-icon");
 // timelineContent.forEach((el) => observer.observe(el));
 timelineAnimate.forEach((el) => observer.observe(el));
